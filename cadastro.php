9ee8f82821ede8e4aaa8078adf5d5182ee44e58e
João Ruffino <131279015+JoaoRuffino@users.noreply.github.com>
2023-10-31 22:03:59 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<?php
+
+
+include_once('config.php');
+
+if(isset($_POST['btncadastrar'])){
+
+    $cliNome = filter_input(INPUT_POST, 'txtnome');
+    $cliTelefone = filter_input(INPUT_POST, 'txtfone');
+    $cliDataNasc = filter_input(INPUT_POST, 'txtdatanasc');
+    
+    $query = $conexao->prepare('INSERT INTO tab clientes (cliNome, cliTelefone, cliDataNasc) VALUES (:cliNome, :cliTelefone, :cliDataNasc)');
+    
+    $query->bindvalue(':cliNome', $cliNome, PDO::PARAM_STR);
+    $query->bindvalue(':cliTelefone', $cliTelefone, PDO::PARAM_STR);
+    $query->bindvalue(':cliDataNasc', $cliDataNasc, PDO::PARAM_STR);
+
+    $query->execute();
+
+}
+?>
+
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+try{
+    $conexao = new PDO('mysql:host=localhost;dbname=clientes', 'root', '');
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conectado ao banco de dados com sucesso <br>";
+} catch(PDOException $e){
+    echo 'Erro na conexão com o banco de dados: ' . $e->getMessage();
+}
+
+?>
